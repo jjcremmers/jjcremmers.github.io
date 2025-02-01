7040de11ed96f427ccad0eee6c1d036b5b1cfd37
jjcremmers <j.j.c.remmers@tue.nl>
2025-02-01 20:34:03 +0000

    Deploying to gh-pages from @ jjcremmers/jjcremmers.github.io@68fdb95d32f2a53c1a8d109ab18fc94284e196bd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,35 +492,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A.%6A.%63.%72%65%6D%6D%65%72%73@%74%75%65.%6E%6C", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://orcid.org/0000-0001-5823-2558", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-researchgate',
+        title: 'ResearchGate',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.researchgate.net/profile/Joris-Remmers/", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=C5l2Pb8AAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-work',
+        title: 'Work',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.tue.nl/en/research/researchers/joris-remmers", "_blank");
         },
       },{
       id: 'light-theme',
